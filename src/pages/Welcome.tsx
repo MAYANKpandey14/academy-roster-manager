08c5f6aeaf415f2a8fbebdab9b9c2b5c55e1d2e9
MAYANKpandey14 <mayankpandeyofficial404@gmail.com>
2025-04-28 22:54:33 +0530

    welcome page footer + font route

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "@/components/languageswitch/LanguageSwitcher";
+import { Mail,Phone } from 'lucide-react';
 
 const Welcome = () => {
     const location = useLocation();
@@ -36,7 +37,7 @@ const Welcome = () => {
             <div className="absolute top-4 right-4 z-10">
                 <LanguageSwitcher />
             </div>
-            
+
             {/* Background div */}
             <div
                 className="absolute inset-0 w-full h-full "
@@ -62,11 +63,18 @@ const Welcome = () => {
                 </h1>
                 <button
                     onClick={handleNext}
-                    className="m-2 px-4 sm:px-6 py-2 bg-blue-600 border-2 border-blue-600 hover:bg-white hover:text-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    className="m-4 px-4 sm:px-6 py-2 bg-blue-600 border-2 border-blue-600 hover:bg-white hover:text-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
                     aria-label={t("openDashboard")}
                 >
                     {t("openDashboard")}
                 </button>
+            </div>
+            <div className="flex flex-col items-center align-bottom text-center text-gray-600">
+                <p className="font-light m-2 text-md">Made by - Mayank Pandey, BTECH CSE, Freelance Full Stack Developer</p>
+                <div className="flex justify-center space-x-4 text-black">
+                <a href="mailto:'mayankpandeyofficial404@gmail.com'" className="text-md"><Mail></Mail></a>
+                <a href="tel:'+917505728099'" className="text-md"><Phone></Phone></a>
+                </div>
             </div>
         </div>
     );
